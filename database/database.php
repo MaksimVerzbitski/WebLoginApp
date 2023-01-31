d344521efb76596b770a01a57ccaf314a6624cf6
MaksimVerzbitski <maksim.verzbitski@gmail.com>
2023-01-31 09:46:00 +0200

    Changing Database Structure

--- database/database.php
+++ database/database.php
@@ -1,19 +1,24 @@
 
-<?php session_start();
-
-$databaseHost='whserver.tptlive.ee';
-$databaseUser="tpqaqkwg_Franix90";
-$databasePassword='5H6rEn2pZt3VGmQ'; 
+<?php
+//session_start();
 //$databaseUser='root';
 //$databaseHost='127.0.0.1';
 //$databasePassword=''; 
+$databaseHost='whserver.tptlive.ee';
+$databaseUser="tpqaqkwg_Franix90";
+$databasePassword='5H6rEn2pZt3VGmQ'; 
 
 $databaseName='tpqaqkwg_votes';
-try {
+/* try {
   $conn = new PDO("mysql:host=".$databaseHost.";dbname=".$databaseName, $databaseUser, $databasePassword);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo 'Database Connected Successfully'; 
 } catch(PDOException $error) {
   echo "Something went wrong " . $error->getMessage();
+} */
+$conn = mysqli_connect($databaseHost, $databaseUser, $databasePassword, $databaseName);
+if(!$conn){
+  die("Something went wrong");
 }
+
 ?>
